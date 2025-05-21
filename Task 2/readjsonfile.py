838ab8f0e17dd81cc2a2d05f65444d549312a3c4
Vinit0812 <vinitdhabaliya08@gmail.com>
2025-05-21 11:34:38 +0530

    Update readjsonfile.py

--- Task 2/readjsonfile.py
+++ Task 2/readjsonfile.py
@@ -2,12 +2,9 @@ import json
 import sys
 
 def read_json_file(filename):
-    try:
-        with open(filename,'r') as file:
-            data = json.load(file)
-            print("json content:",data)
-    except(FileNotFoundError,json.JSONDecodeError):
-        print(f"error,{filename}")
+    with open(filename,'r') as file:
+        data = json.load(file)
+        print("json content:",data)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
